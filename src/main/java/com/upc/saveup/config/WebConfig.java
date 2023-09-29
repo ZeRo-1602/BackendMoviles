a0019c51480fef0325b11d3d7cc9a451754ad761
ZeRo-1602 <persebas11@gmail.com>
2023-09-29 12:39:33 -0500

    Update WebConfig.java

--- src/main/java/com/upc/saveup/config/WebConfig.java
+++ src/main/java/com/upc/saveup/config/WebConfig.java
@@ -9,9 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-
+                .allowedOrigins("http://backendmoviles-production.up.railway.app/", "https://backendmoviles-production.up.railway.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
-
-
     }
 }
