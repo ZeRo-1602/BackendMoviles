dc41730e26af009f7e7f8c4fd93815fa5bdb76a8
ZeRo-1602 <persebas11@gmail.com>
2023-09-29 12:13:43 -0500

    Update WebConfig.java

--- src/main/java/com/upc/saveup/config/WebConfig.java
+++ src/main/java/com/upc/saveup/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
 
 
     }
